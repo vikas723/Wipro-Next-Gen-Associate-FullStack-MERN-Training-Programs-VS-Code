713b4deb2829694cb1bdbeec70eea3aaf48c38b4
vikas723 <20epci045@skcet.ac.in>
2026-01-23 11:42:57 +0530

    CSS Transition Group

---
+++ Advanced ReactJS/routing/src/components/Animation.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Routes, Route, useLocation} from "react-router-dom"
+import{useRef} from "react"
+import {CSSTransition, TransitionGroup} from 'react-transition-group'
+
+import Home from '../pages/Home'
+import Employee from '../pages/Employee'
+function Animation() {
+    const location =  useLocation();
+    const nodeRef = useRef(null); // useRef is used to remeber the DOM Element, becuase when transition tries to animte then it wont find dom element so react throes a transition error, Reference is null
+  return (
+    <div>
+        <TransitionGroup>
+            <CSSTransition key={location.pathname} classNames="fade" timeout ={300} nodeRef = {nodeRef}>
+                <div ref={nodeRef}>
+                <Routes location={location}>
+                    <Route path ="/" element={<Home/>}/>
+                    <Route path = "/employees" element={<Employee/>}/>
+                </Routes>
+                </div>
+            </CSSTransition>
+        </TransitionGroup>
+      
+    </div>
+  )
+}
+
+export default Animation
