419652ce7cc97557344dd833cb58021b218e5f9c
vikas723 <20epci045@skcet.ac.in>
2026-02-03 14:06:30 +0530

    Day 19 - Events , Async Events, CallBack, Promises

--- Day 19 - Events , Async Events, CallBack, Promises, Express JS/events/callback.js
+++
@@ -1,9 +0,0 @@
-function processPayment(amount, callback){
-    setTimeout(()=>{
-        callback("Payment Successful");
-    }, 3000);
-}
-console.log("example");
-processPayment(5500, (msg) =>{
-    console.log(msg);
-})
