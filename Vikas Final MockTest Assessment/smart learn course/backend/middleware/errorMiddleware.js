f5cbfc3ee39c3dd5ae0f2d32835d3a657cbc1742
vikas723 <81210333+vikas723@users.noreply.github.com>
2026-02-20 16:17:49 +0530

    Delete Vikas Final MockTest Assessment/smart learn course directory

--- Vikas Final MockTest Assessment/smart learn course/backend/middleware/errorMiddleware.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  res.status(500).json({
-    success: false,
-    message: err.message || "Server Error"
-  });
-};
-
-module.exports = errorHandler;
--- Vikas Final MockTest Assessment/smart learn course/frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: "http://localhost:5000/api"
-});
-
-export const fetchCourses = () => API.get("/courses");
-export const enrollCourse = (data) => API.post("/enroll", data);
